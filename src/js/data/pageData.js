5d6d09f037f5e938471c3859357564905bd4f769
Krzysztof <kpiekarz1983@gmail.com>
2019-12-09 21:48:18 +0100

    Added services section

--- src/js/data/pageData.js
+++ src/js/data/pageData.js
@@ -4,5 +4,10 @@ module.exports = {
     bigText: 'Welcome To Our Studio!',
     headingPrimary: 'It’s nice to Meet you',
     button: 'Tell Me More'
+  },
+  services: {
+    headingSecondary: 'Services',
+    prime: 'Proin iaculis purus consequat sem cure.',
+    features: ['E-Commerce', 'Web Security', 'Responsive Web']
   }
 };
